45ad3683407d3003495231e2fa307756ec3285aa
우유진 <glem1004@gmail.com>
2019-10-02 18:59:56 +0900

    [파일 이름 변경] home.jsp -> intro.jsp
    [폴더추가]
    views->search
    
    [컨트롤러 return값 변경]
    return home -> return intro

--- src/main/java/com/kosmo/workout/HomeController.java
+++ src/main/java/com/kosmo/workout/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "home";
+		return "intro";
 	}
 	@RequestMapping("/main.do")
 	public String main() {
