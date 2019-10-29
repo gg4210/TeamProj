5e47b178f032ed3ef5b884b386edfec44dde876c
김태윤 <proyectil@naver.com>
2019-10-29 14:28:00 +0900

    중국달력

---
+++ src/main/webapp/resources/versatile-date-time-month-year-picker/js/datepicker.en.js
@@ -0,0 +1,26 @@
+/**
+ * Simplified Chinese translation for bootstrap-datetimepicker
+ * Yuan Cheung <advanimal@gmail.com>
+ */
+;(function($){
+	$.fn.datePicker.dates['en'] = {
+		days: ["일", '월', '화', '수', '목', '금', '토'],
+		months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+		now: "오늘",
+		clear: '취소',
+		headerYearLink:'',
+		units: ['-', ''],
+		button: ["선택", "취소"],
+		confirm: '선택',
+		cancel: '취소',
+		chooseDay: '날짜 선택',
+		chooseTime: '시간 선택',
+		begin: '시작일',
+		end: '종료일',
+		prevYear: '작년',
+		prevMonth: '전월',
+		nextYear: '내년',
+		nextMonth: '다음달',
+		zero: '0:00'
+	};
+}(jQuery));
