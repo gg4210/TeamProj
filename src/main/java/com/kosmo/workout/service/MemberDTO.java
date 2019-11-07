3c286d23a98f33e96e6a98abd7638fbe4250612e
안정준 <richard1500@naver.com>
2019-11-07 17:45:08 +0900

    pull을 위한 커밋

---
+++ src/main/java/com/kosmo/workout/service/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.kosmo.workout.service;
+
+public class MemberDTO {
+	private String Id;
+	private String Password;
+	private String Name;
+	private String Email;
+	private String Address;
+	private String Cellphone;
+	private String Authority;
+	private String MyComment;
+	private String Picture;
+	private java.sql.Date JoinDate;
+	
+	
+}
