02a219b77dfb0ddbe32149344724421b758c3f00
우유진 <glem1004@gmail.com>
2019-11-09 19:09:45 +0900

    [변경사항] pom.xml에서 셀레니움, JSOUP에 필요한 메이븐 디펜던시 추가하였음. 다운받는데 시간 걸림. 2. Controller : CommonUtility.class 셀레니움, Jsoup을 이용한 웹크롤링 ing 			(현재 네이버에서 필요한 정보를 긁어오는 것은 가능. 			배열부분을 긁을 방법은 고민중.

--- src/main/java/com/kosmo/workout/util/CommonUtility.java
+++ src/main/java/com/kosmo/workout/util/CommonUtility.java
@@ -1,17 +1,68 @@
 package com.kosmo.workout.util;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.json.simple.JSONArray;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 public class CommonUtility {
 
 	
-	
+	public static JSONArray mapkeyCrawling(String mapkey, HttpServletRequest req) throws IOException {
+		
+		//맵키로 상호명 구함
+		String base_url="https://place.map.kakao.com/"+mapkey;
+		Document doc=Jsoup.connect(base_url).get();
+		Elements result=doc.select("head > meta:nth-child(4)");
+		String title=result.get(0).attr("content");
+		//맵키로 상호명 구함 끝
+		
+		//네이버로 검색
+		base_url="https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query="+title;
+		doc=Jsoup.connect(base_url).get();
+		result=doc.select("#sp_local_1 > div.tit_area > a");
+		String href=result.get(0).attr("href");
+		//네이버 검색 끝
+		
+		
+		//검색으로 나오는 주소로 셀레니움 웹크롤링 시작
+		String path=req.getSession().getServletContext().getRealPath("/");
+		String webDriverPath=path+"resources"+File.separator+"webdriver"+File.separator+"chromedriver.exe";
+		//드라이버 셋업
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("ignoreProtectedModeSettings", true);
+        options.setHeadless(true);
+        WebDriver driver= new ChromeDriver(options);	
+		System.setProperty("webdriver.chrome.driver", webDriverPath);
+		//드라이버 셋업 끝
+		
+		//주소에 있는 모든 내용 웹크롤링(하는 중)
+		driver.get(href);
+		String addr=driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[2]/dl/dd[1]/a")).getText();
+		String jibunAddr=driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[2]/dl/dd[1]/p/a")).getText();
+		String tel=driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[2]/dl/dd[2]")).getText();
+		
+		System.out.println(String.format("주소:%s,지번주소:%s,전화번호:%s", addr,jibunAddr,tel));
+		
+		
+		driver.quit();
+        
+
+		return null;
+	}
 	
 	
 }
--- src/main/java/com/kosmo/workout/web/SearchController.java
+++ src/main/java/com/kosmo/workout/web/SearchController.java
@@ -3,32 +3,27 @@ package com.kosmo.workout.web;
 import java.io.IOException;
 import java.util.Map;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kosmo.workout.util.CommonUtility;
+
 @Controller
 public class SearchController {
+	
 
 	@RequestMapping("/searchList.do")
 	public String searchList(@RequestParam Map map) {
-		
-		return "search/list.tiles";
-		
+		return "search/list.tiles";	
 	}
 	
 	@RequestMapping("/searchView.do")
-	public String searchView(@RequestParam Map map) throws IOException {
+	public String searchView(@RequestParam("mapkey") String mapkey, HttpServletRequest req) throws IOException {
 		
-		Document doc = Jsoup.connect("https://place.map.kakao.com/"+map.get("mapkey")).get();
-		Elements first=doc.select("head > meta:nth-child(4)");
-		String title=first.get(0).attr("content");
-		System.out.println(title);
+		CommonUtility.mapkeyCrawling(mapkey,req);
 		
 		return "search/view.tiles";
 	}
