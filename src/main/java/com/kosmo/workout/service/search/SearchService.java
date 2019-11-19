af17fd608b94561b9c9e8ce61294a0227a38ebb2
우유진 <glem1004@gmail.com>
2019-11-19 12:25:07 +0900

    [변경사항]
    맵핑 오류 수정

--- src/main/java/com/kosmo/workout/service/search/SearchService.java
+++ src/main/java/com/kosmo/workout/service/search/SearchService.java
@@ -5,20 +5,20 @@ import java.util.Map;
 public interface SearchService {
 
 	// 센터 정보 유무 확인용
-	int isIn(String mapkey);
+	int isIn(Map map);
 	// 센터 정보 유무 확인용
 	
 	//센터 정보 입력, 수정, 불러오기 관련//
 	SearchBBSDTO insertSearchDTO(Map map);
 	SearchBBSDTO updateSearchDTO(Map map);	
-	SearchBBSDTO selectOneSearchDTO(String mapkey);
+	SearchBBSDTO selectOneSearchDTO(Map map);
 	//센터 정보 입력, 수정 관련 끝//
 	
 	//코멘트 입력 , 리스트 뿌려주기 시작//
 	SearchBBSCommentDTO insertComment(Map map);
 	SearchBBSCommentDTO selectListComment(Map map);
-	SearchBBSCommentDTO DeleteListComment(String id);
-	SearchBBSCommentDTO updateListComment(String id);
+	SearchBBSCommentDTO DeleteListComment(Map map);
+	SearchBBSCommentDTO updateListComment(Map map);
 	//코멘트 입력 , 리스트 뿌려주기 끝//
 	
 }
